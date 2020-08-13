d46ef4fc3d9b47e96e2c495c10181db5b136e36e
yiminghu <yiminghu@tencent.com>
2020-08-13 09:32:02 +0800

    ci: mac deploy to wetest

--- scripts/Deployqt.sh
+++ scripts/Deployqt.sh
@@ -1,8 +1,15 @@
 set -xeuo pipefail
 echo Deployqt.....
 pwd
+pushd .
 cd $ReleasePath
 cd ../
 $MacdeployqtPath LoliProfiler.app -dmg -always-overwrite
+
+popd
+
+mkdir $DeployPath
+
+cp $ReleasePath/../LoliProfiler.dmg $DeployPath
 
 echo finish Deployqt.....
--- scripts/EnterPoint.sh
+++ scripts/EnterPoint.sh
@@ -4,8 +4,10 @@ export AndroidPluginPath="./plugins/Android"
 export ReleasePath="./build/cmake/bin/release/LoliProfiler.app"
 export MacdeployqtPath=`find $QT5Path -name "macdeployqt"`
 export QT5_Clang=`find $QT5Path -name "clang_64"`
+export DeployPath="./dist"
 echo deleting dir
 rm -rf ./build
+rm -rf ./dist
 
 
 sh scripts/BuildProject.sh
